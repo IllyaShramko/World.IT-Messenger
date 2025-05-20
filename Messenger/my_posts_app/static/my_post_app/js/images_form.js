48e0f42b1ee7f0bf52d9efb54ccd922f93bf55b5
IllyaShramko <123illya123123r@gmail.com>
2025-05-20 13:06:01 +0300

    Added delete button for uploaded image in form

--- Messenger/my_posts_app/static/my_post_app/js/images_form.js
+++ Messenger/my_posts_app/static/my_post_app/js/images_form.js
@@ -1,18 +1,19 @@
 const blockImages = document.querySelector('.modal-images')
 const inputImages = document.getElementsByName("images")[0]
+let imageElement = document.createElement("img")
+let imageDelete = document.createElement("img")
+let divImageDelete = document.createElement("button")
 
 inputImages.addEventListener('change', function(event) {
     const image = event.target.files[0];
     if (image) {
         const reader = new FileReader();
         reader.addEventListener('load', function(e) {
-            let imageElement = document.createElement("img")
-            let imageDelete = document.createElement("img")
-            let divImageDelete = document.createElement("button")
             imageElement.src = e.target.result
             imageElement.className = "preview-img"
             imageDelete.src = "/static/my_post_app/images/delete.png"
             divImageDelete.className = "preview-delete-img"
+            divImageDelete.id = "deleteImage"
             divImageDelete.type = "button"
             blockImages.appendChild(imageElement)
             blockImages.appendChild(divImageDelete)
@@ -21,3 +22,13 @@ inputImages.addEventListener('change', function(event) {
         reader.readAsDataURL(image);
     }
 });
+
+divImageDelete.addEventListener(
+    "click",
+    function () {
+        console.log("dslfdkfasdf")
+        inputImages.value = ""
+        imageElement.remove()
+        divImageDelete.remove()
+    }
+)
